b4e515d8c203539a85b5fddc52d8bb359df981dd
Anisxaman <83417019+Anisxaman@users.noreply.github.com>
2021-09-14 03:39:45 +0600

    third

--- js/app.js
+++ js/app.js
@@ -16,15 +16,27 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product p-2 rounded-3 me-3 mt-4" style="height:500px; background:#fbfcfd;"  >
+    div.innerHTML = `<div class="single-product p-2 rounded-3 me-3 mt-4" style="height:600px; background:#fbfcfd;"  >
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image img-fluid" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <br>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <div>
+      <div>
+        <h4 class="mt-3">Mens Casual Premium Slim Fit T-Shirts </h4>
+        <p class="mt-2 "><span class="fw-bold">Category:</span> men's clothing</p>
+        <br>
+        <h2><span class="text-danger">Price:</span> $ 22.3</h2>
+       </div>
+
+        <div class="d-flex justify-content-between mt-2 mb-2 ps-3 pe-3">
+          <h5 class="mt-3"><span class="text-danger">Rating:</span>${product.rating.rate}</h5>
+          <h5 class="mt-3"><span class="text-danger">Rating count:</span>${product.rating.count}</h5>
+    
+         
+    
+        </div>
+    </div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
@@ -41,7 +53,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -50,12 +62,14 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
+  updateTotal();
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  // document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText =value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -73,12 +87,16 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+  updateTotal();
 };
 
 //grandTotal update function
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+
+  // const price=document.getElementById("price").value;
+  // console.log(price);
+
+  const grandTotal = getInputValue("price") + getInputValue("delivery-charge")+getInputValue("total-tax");
+    console.log(grandTotal);
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
